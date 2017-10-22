b1f335aebae7b6f2c1df0f5139c60f4a3024780d
arvind0598 <arvind0598@gmail.com>
2017-10-22 12:29:35 +0530

    Updated script.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
-  //$.getJSON("https://api.openweathermap.org/data")
-  console.log("ready");
   if(navigator.geolocation)
     {
-      console.log("here");
       navigator.geolocation.getCurrentPosition(function(pos){
-        console.log("latitude: " + pos.coords.latitude + "<br>longitude: " + pos.coords.longitude);
+        console.log("obtained location");
+        var latlongstr = pos.coords.latitude + "," + pos.coords.longitude;
+        $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+        latlongstr + "&key=AIzaSyB0meAxHItFeYsIhRKBEBEe9I9dmbMfLbs", function(json)
+        {
+          if(json.status == "OK") console.log("results returned");
+          else console.log("error: " + JSON.stringify(json.status));
+          console.log(json);
+          var name = json.results[0]["address_components"];
+          for(i in name) if(name[i].types[0] == "locality") console.log(name[i]);              
+        });
       });
     }
+  else console.log("could not obtain location");
 })
 
-//AIzaSyB0meAxHItFeYsIhRKBEBEe9I9dmbMfLbs
+//AIzaSyB0meAxHItFeYsIhRKBEBEe9I9dmbMfLbs googlemaps
+
