dc4977d47212c56d404abad35572ad50dd828efd
arvind0598 <arvind0598@gmail.com>
2017-12-15 14:36:58 +0530

    Routine commit.

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ $(document).ready(function(){
   if(navigator.geolocation)
     {
       navigator.geolocation.getCurrentPosition(function(pos){
-        console.log("obtained location");
-        var latlongstr = pos.coords.latitude + "," + pos.coords.longitude;
-        $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
-        latlongstr + "&key=AIzaSyB0meAxHItFeYsIhRKBEBEe9I9dmbMfLbs", function(json)
-        {
-          if(json.status == "OK") console.log("results returned");
-          else console.log("error: " + JSON.stringify(json.status));
-          console.log(json);
-          var name = json.results[0]["address_components"];
-          for(i in name) if(name[i].types[0] == "locality") console.log(name[i]);              
-        });
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude + "&APPID=a8abd2b0a72ed937ddbcbd43fceeb2cc", function(json)
+          {
+            var cel = parseFloat(json.main.temp) - 273.15;
+            var far = cel * 1.8 + 32;
+
+            $("#loc").text(json.name);
+            $("#feels").text(json.weather[0].main);
+            $("#temp").text(cel.toFixed(2) + " Celsius");
+            $("#icon").html("<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+
+            document.querySelector("input[type=checkbox]").addEventListener("change", function()
+            {
+              if($("#ff").prop('checked')) $("#temp").text(far.toFixed(2) + " " + "Farenheit");
+              else $("#temp").text(cel.toFixed(2) + " " + "Celsius");
+            });
+          });
       });
     }
   else console.log("could not obtain location");
 })
 
-//AIzaSyB0meAxHItFeYsIhRKBEBEe9I9dmbMfLbs googlemaps
+//a8abd2b0a72ed937ddbcbd43fceeb2cc openweathermaps
 
