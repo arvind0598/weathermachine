c87bb5d2edf1933557d8a6e21775faa09058e1c7
Arvind S <arvind0598@gmail.com>
2019-10-19 21:59:50 +0530

    setup project with parcel and vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div>
+        <h1> The Weather Machine </h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+/* body {
+    background: red;
+} */
+</style>
