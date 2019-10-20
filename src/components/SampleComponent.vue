09f1b1e8947e012367c3c42c6b791881ccaa4482
Arvind S <arvind0598@gmail.com>
2019-10-20 10:44:15 +0530

    added a sample component

---
+++ src/components/SampleComponent.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div>
+        <h1> Sample Component </h1>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {};
+  },
+});
+</script>
