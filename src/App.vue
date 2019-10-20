aefd91f2757aef913ea93d0cc27432770fbb511a
Arvind S <arvind0598@gmail.com>
2019-10-20 10:44:33 +0530

    hooked up scripts and sample component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template lang="html">
     <div>
         <h1> The Weather Machine </h1>
+        <sample-component/>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import SampleComponent from './components/SampleComponent.vue';
+import getUserLocation from './scripts/getUserLocation';
+import fetchWeatherData from './scripts/fetchWeatherData';
 
 export default Vue.extend({
   data() {
     return {};
+  },
+  components: {
+    SampleComponent,
+  },
+  beforeMount: () => {
+    getUserLocation(fetchWeatherData, null);
   },
 });
 </script>
