ce24b7bf1ff09be3a2f40a7baf700f126a79cf41
Arvind S <arvind0598@gmail.com>
2019-10-29 00:10:49 +0530

    added helpful messages to the loading card

--- src/components/CardComponent/CardFrame.tsx
+++ src/components/CardComponent/CardFrame.tsx
@@ -24,7 +24,7 @@ const displayCardContent = (data: CardFrameProps) => {
       />
     );
   }
-  return <LoadingCard />;
+  return <LoadingCard status={data.status} />;
 };
 
 const displaySwitchComponent = (status: LocationFetchStatus) => {
--- src/components/CardComponent/LoadingCard.tsx
+++ src/components/CardComponent/LoadingCard.tsx
@@ -1,18 +1,43 @@
 import React, { CSSProperties } from 'react';
+import { LocationFetchStatus } from '../../types';
 
 const flexStyle: CSSProperties = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  flexDirection: 'column',
 };
 
-const LoadingCard = () => (
-  <div className="content" style={flexStyle}>
-    <div className="lds-ripple">
-      <div />
-      <div />
+type LoadingCardProps = {
+  status: LocationFetchStatus;
+};
+
+const displayMessage = (status: LocationFetchStatus): string => {
+  switch (status) {
+    case 'DENIED':
+      return 'Permission for location was denied.';
+    case 'FETCHING':
+      return 'Please wait while we fetch weather data.';
+    case 'FETCH_FAILED':
+      return 'There was an error while fetching data. Please try again later.';
+    case 'REQUESTING':
+      return 'Please provde location permissions to continue.';
+    default:
+      return null;
+  }
+};
+
+const LoadingCard = (props: LoadingCardProps) => {
+  const { status } = props;
+  return (
+    <div className="content" style={flexStyle}>
+      <div className="lds-ripple">
+        <div />
+        <div />
+      </div>
+      <p className="subtitle is-6 has-text-centered"> {displayMessage(status)} </p>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoadingCard;
